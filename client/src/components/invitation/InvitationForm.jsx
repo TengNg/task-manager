7eb5cb9aef39c6d6d6bf902484e7c0e50092d463
= <nguyentin148@gmail.com>
2023-12-17 18:16:53 +0700

    fix(ui): adding new styles + remove some unused

--- client/src/components/invitation/InvitationForm.jsx
+++ client/src/components/invitation/InvitationForm.jsx
@@ -77,7 +77,7 @@ const InvitationForm = ({ setOpen }) => {
                 className="fixed box-border top-0 left-0 text-gray-600 font-bold h-[100vh] text-[1.25rem] w-full bg-gray-500 opacity-40 z-50 cursor-auto">
             </div>
 
-            <div className="fixed box--style flex flex-col items-start py-3 px-10 top-[5rem] right-0 left-[50%] -translate-x-[50%] w-fit min-w-[400px] h-[300px] min-h-[300px] border-black border-[2px] z-50 cursor-auto bg-gray-200">
+            <div className="fixed box--style flex flex-col items-start pt-3 pb-6 px-8 top-[5rem] right-0 left-[50%] -translate-x-[50%] w-fit min-w-[400px] max-h-[500px] min-h-[300px] border-black border-[2px] z-50 cursor-auto bg-gray-200">
                 <Loading loading={loading} />
 
                 <button
@@ -89,59 +89,64 @@ const InvitationForm = ({ setOpen }) => {
 
                 <p className="mt-2 mb-4 font-normal text-[1rem] text-gray-700">Invite people this board</p>
 
-                <div className="w-full flex items-center gap-2">
+                <div className="w-full flex flex-wrap items-center gap-3 pb-1">
                     <input
                         ref={usernameInputRef}
-                        className={`h-full p-2 overflow-hidden whitespace-nowrap text-ellipsis border-[3px] bg-gray-100 border-gray-600 text-gray-600 font-bold select-none font-mono focus:outline-none`}
-                        placeholder="username"
+                        className={`p-3 w-full overflow-hidden shadow-[0_3px_0_0] shadow-gray-600 text-[0.75rem] whitespace-nowrap text-ellipsis border-[2px] bg-gray-100 border-gray-600 text-gray-600 font-bold select-none font-mono focus:outline-none`}
+                        placeholder="Enter username..."
                         onChange={(e) => setUsername(e.target.value)}
                         value={username}
                     />
                     <button
                         onClick={() => handleSendInvitation()}
-                        className="button--style h-full">+ invite</button>
+                        className="button--style border-[2px] py-2 text-[0.75rem] hover:bg-gray-600 hover:text-white transition-all"
+                    >
+                        + invite
+                    </button>
                 </div>
 
-                <p className="text-center h-3 text-red-700 text-[0.75rem] font-semibold">{errMsg}</p>
+                <p className="text-center h-3 text-red-700 text-[0.65rem] font-semibold mb-1">{errMsg}</p>
 
-                <div className="flex flex-col mt-[1rem] gap-3 flex-wrap w-full mx-auto">
+                <div className="flex flex-col gap-3 w-full max-w-[400px] overflow-scroll border-[1px] border-t-gray-600 pt-4">
                     <div className="flex gap-1">
-                        {/* <div className='bg-blue-500 text-white flex--center text-[0.8rem] w-[40px] h-[40px] rounded-full bg-center bg-cover overflow-hidden cursor-pointer'> */}
-                        {/*     <div className="font-bold flex--center select-none">{boardState.board.createdBy.username.charAt(0).toUpperCase()}</div> */}
-                        {/* </div> */}
-
                         <Avatar
                             username={boardState.board.createdBy.username}
                             profileImage={boardState.board.createdBy.profileImage}
                             size="md"
+                            clickable={false}
                         />
 
                         <div className="flex flex-col justify-center">
-                            <p className="text-[0.9rem] font-semibold">{boardState.board.createdBy.username} {auth?.username === boardState.board.createdBy.username && '(you)'}</p>
-                            <p className="text-[0.75rem]">Owner</p>
+                            <p className="text-[0.65rem] text-gray-800 font-semibold">{boardState.board.createdBy.username} {auth?.username === boardState.board.createdBy.username && '(you)'}</p>
+                            <p className="text-[0.65rem] text-gray-800">Owner</p>
                         </div>
                     </div>
 
                     {
-                        boardState.board.members.map((user, index) => {
-                            return <div key={user._id} className="flex gap-1">
-                                <div className="flex gap-1 flex-1">
-                                    <Avatar
-                                        username={user.username}
-                                        profileImage={user.profileImage}
-                                        size="md"
-                                    />
-                                    <div className="flex flex-col justify-center">
-                                        <p className="text-[0.9rem] font-semibold">{user.username} {auth?.username === user.username && '(you)'}</p>
-                                        <p className="text-[0.75rem]">Member</p>
+                        boardState.board.members.map((user, _) => {
+                            return <>
+                                <div key={user._id} className="flex gap-1">
+                                    <div className="flex gap-1 flex-1">
+                                        <Avatar
+                                            username={user.username}
+                                            profileImage={user.profileImage}
+                                            size="md"
+                                            clickable={false}
+                                        />
+                                        <div className="flex flex-col justify-center">
+                                            <p className="text-[0.65rem] text-gray-800 font-semibold">{user.username} {auth?.username === user.username && '(you)'}</p>
+                                            <p className="text-[0.65rem] text-gray-800">Member</p>
+                                        </div>
                                     </div>
+                                    {
+                                        boardState.board.createdBy.username === auth.username
+                                        && <button
+                                            onClick={() => handleRemoveMemberFromBoard(user._id)}
+                                            className="text-[0.65rem] button--style border-[2px] hover:bg-gray-600 hover:text-white transition-all">
+                                            Remove from board
+                                        </button>}
                                 </div>
-                                {
-                                    boardState.board.createdBy.username === auth.username
-                                    && <button
-                                        onClick={() => handleRemoveMemberFromBoard(user._id)}
-                                        className="text-[0.75rem] button--style">Remove from board</button>}
-                            </div>
+                            </>
                         })
                     }
                 </div>
