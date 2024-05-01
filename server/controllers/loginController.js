bdbc4ed8cd8613b62827d99ff12170688858313e
= <nguyentin148@gmail.com>
2024-05-01 15:08:12 +0700

    feat: set log-in state to 2 days

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -36,7 +36,7 @@ const handleLogin = async (req, res) => {
             httpOnly: true,
             sameSite: 'None',
             secure: true,
-            maxAge: 24 * 60 * 60 * 1000
+            maxAge: 2 * 24 * 60 * 60 * 1000
         }
     );
 
