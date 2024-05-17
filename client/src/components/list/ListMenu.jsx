de6ea96b73646ccddadc28c54aee645d9fc9c963
= <nguyentin148@gmail.com>
2024-05-17 07:26:22 +0700

    feat(move-list-form): set default board value

--- client/src/components/list/ListMenu.jsx
+++ client/src/components/list/ListMenu.jsx
@@ -57,12 +57,12 @@ export default function ListMenu({ list, setOpen, handleDelete, handleCopy, proc
             tabIndex={-1}
             ref={containerRef}
             onBlur={closeOnBlur}
-            className={`absolute top-0 left-0 outline-none z-10 bg-gray-100 border-gray-600 border-[2px] shadow-gray-600 box--style w-full p-3 ${theme.itemTheme == 'rounded' ? 'rounded-md shadow-[0_4px_0_0]' : 'shadow-[4px_6px_0_0]'}`}
+            className={`absolute top-0 left-0 outline-none z-10 bg-slate-200 border-gray-600 border-[2px] shadow-gray-600 box--style w-full p-3 ${theme.itemTheme == 'rounded' ? 'rounded-md shadow-[0_4px_0_0]' : 'shadow-[4px_6px_0_0]'}`}
         >
 
             <div className='border-b-[1px] border-b-black pb-2'>
-                <div className='text-[9px] sm:text-[0.75rem]'>title: <span className='font-medium underline'>{list.title}</span></div>
-                <div className='text-[9px] sm:text-[0.75rem]'>created at: {dateFormatter(list.createdAt)}</div>
+                <div className='text-[9px] sm:text-[0.8rem]'>title: <span className='font-medium underline'>{list.title}</span></div>
+                <div className='text-[9px] sm:text-[0.8rem] mt-1'>created at: {dateFormatter(list.createdAt)}</div>
             </div>
             <div className='flex flex-col gap-3 mt-3'>
                 <button
