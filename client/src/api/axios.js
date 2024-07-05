5dc81e4bc83cd07ad17fdd60c5234c3a30e56bd1
TengNg <nguyentin148@gmail.com>
2024-07-05 10:48:24 +0700

    chore: unused axios instances

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,16 +2,6 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001';
 
-export default axios.create({
-    baseURL: `${BASE_URL}`,
-    withCredentials: true, // private route
-    headers: { 'Content-Type': 'application/json' }
-});
-
-export const testAxios = axios.create({
-    baseURL: `${BASE_URL}/test`,
-});
-
 export const axiosPrivate = axios.create({
     baseURL: `${BASE_URL}`,
     withCredentials: true, // private route
